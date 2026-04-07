58a976e09baf7916b3d26f876e55db92fe241abc
RahulG-12 <rahulg-12@users.noreply.github.com>
2026-04-08 01:18:47 +0530

    fix cors

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.core.database import engine, Base
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 origins = [
-    "https://finvault-drab.vercel.app/",  # your frontend
+    "https://finvault-drab.vercel.app",  # your frontend
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
